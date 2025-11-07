0aae10c4ddee6402a3b04273f0b6f875017d1aad
AmitKirar <amitkirar8889@gmail.com>
2025-11-07 18:58:35 +0530

    done

---
+++ spring-dao-hibernate/src/main/java/com/rays/service/UserServiceHibImpl.java
@@ -0,0 +1,62 @@
+package com.rays.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rays.dao.UserDAOInt;
+import com.rays.dto.UserDTO;
+
+@Service
+public class UserServiceHibImpl implements UserServiceInt {
+
+	@Autowired
+	private UserDAOInt dao = null;
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public long add(UserDTO dto) {
+		long pk = dao.add(dto);
+		return pk;
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void delete(UserDTO dto) {
+		dao.delete(dto);
+
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void update(UserDTO dto) {
+		dao.update(dto);
+
+	}
+
+	@Transactional(readOnly = true)
+	public UserDTO findByPk(long id) {
+		UserDTO dto = dao.findByPk(id);
+		return dto;
+
+	}
+
+	@Transactional(readOnly = true)
+	public UserDTO findByLogin(String login) {
+		UserDTO dto = dao.findByLogin(login);
+		return dto;
+	}
+
+	@Transactional(readOnly = true)
+	public UserDTO authenticate(String login, String password) {
+		UserDTO dto = dao.authenticate(login, password);
+		return dto;
+	}
+
+	@Transactional(readOnly = true)
+	public List search(UserDTO dto, int pageNo, int pageSize) {
+		return dao.search(dto, pageNo, pageSize);
+
+	}
+
+}
---
+++ spring-dao-hibernate/src/main/java/com/rays/test/TestUserService.java
@@ -0,0 +1,127 @@
+package com.rays.test;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import com.rays.dto.UserDTO;
+import com.rays.service.UserServiceInt;
+
+@Component("testUserService")
+public class TestUserService {
+
+	@Autowired
+	private UserServiceInt service = null;
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		TestUserService test = (TestUserService) context.getBean("testUserService");
+
+//		test.testAdd();
+//		test.testDelete();
+//		test.testUpdate();
+//		test.testFindByPk();
+//		test.testFindByLogin();
+//		test.testAuthenticate();
+		test.testSearch();
+	}
+
+	private void testAdd() {
+
+		UserDTO dto = new UserDTO();
+		dto.setFirstName("chetan");
+		dto.setLastName("patidar");
+		dto.setLogin("chetan@gmail.com");
+		dto.setPassword("chetan@123");
+		long pk = service.add(dto);
+		System.out.println("data added successfully..." + pk);
+
+	}
+
+	private void testDelete() {
+
+		UserDTO dto = new UserDTO();
+		dto.setId(3);
+		service.delete(dto);
+		System.out.println("data deleted successfully ");
+
+	}
+
+	private void testUpdate() {
+
+		UserDTO dto = new UserDTO();
+		dto.setId(2);
+		dto.setFirstName("aditya");
+		dto.setLastName("sharma");
+		dto.setLogin("aditya@gmail.com");
+		dto.setPassword("aditya@123");
+		service.update(dto);
+		System.out.println("data updated successfully..");
+	}
+
+	private void testFindByPk() {
+
+		UserDTO dto = new UserDTO();
+		dto = service.findByPk(2);
+
+		if (dto != null) {
+			System.out.print(dto.getId());
+			System.out.print("\t" + dto.getFirstName());
+			System.out.print("\t" + dto.getLastName());
+			System.out.print("\t" + dto.getLogin());
+			System.out.println("\t" + dto.getPassword());
+		}
+	}
+
+	private void testFindByLogin() {
+
+		UserDTO dto = new UserDTO();
+		dto = service.findByLogin("akshat@gmail.com");
+
+		if (dto != null) {
+			System.out.print(dto.getId());
+			System.out.print("\t" + dto.getFirstName());
+			System.out.print("\t" + dto.getLastName());
+			System.out.print("\t" + dto.getLogin());
+			System.out.println("\t" + dto.getPassword());
+		}
+	}
+
+	private void testAuthenticate() {
+		UserDTO dto = new UserDTO();
+		dto = service.authenticate("akshat@gmail.com", "akshat@123");
+
+		if (dto != null) {
+			System.out.print(dto.getId());
+			System.out.print("\t" + dto.getFirstName());
+			System.out.print("\t" + dto.getLastName());
+			System.out.print("\t" + dto.getLogin());
+			System.out.println("\t" + dto.getPassword());
+		}
+
+	}
+
+	private void testSearch() {
+		List<UserDTO> list = null;
+		UserDTO dto = new UserDTO();
+
+		dto.setFirstName("akshat");
+
+		list = service.search(dto, 1, 5);
+
+		for (UserDTO udto : list) {
+
+			System.out.println(udto.getId());
+			System.out.println(udto.getFirstName());
+			System.out.println(udto.getLastName());
+			System.out.println(udto.getLogin());
+			System.out.println(udto.getPassword());
+		}
+	}
+
+}
